960dc0175977e73b4b6ef8f9116f2afd367490d8
Luis Isea <19-10175@usb.ve>
2023-05-17 21:50:14 -0400

    Añadidos comentarios, retoques de formato y la declaración

--- lab-sem-1/codigo/Main.kt
+++ lab-sem-1/codigo/Main.kt
@@ -1,12 +1,19 @@
-// estaEnOrdenAscendente: Array<Int> -> Boolean
-// Uso: estaEnOrdenAscendente(A)
-// Precondición: A.size > 0
-// Poscondición: Devuelve true si A está ordenado en forma ascendente
+/*
+Laboratorio de la semana 1 de Algoritmos y Estructuras de Datos II (CI-2692)
+Autores: Juan Cuevas (19-10056) y Luis Isea (19-10175)
+*/
+
+/**
+* estaEnOrdenAscendente: Array<Int> -> Boolean
+* Uso: estaEnOrdenAscendente(secuencia)
+* Precondición: secuencia.size > 0
+* Poscondición: Devuelve true si la secuencia está ordenado en forma ascendente
+* y false en caso contrario.
+*/
 fun estaEnOrdenAscendente(secuencia: Array<Int>): Boolean {
-    val n = secuencia.size
-    for (i in 0 until n-1) {
+    for (i in 0 until secuencia.size - 1) {
         // Itera sobre los elementos de la secuencia desde el primero hasta el penúltimo
-	    if (secuencia [i] > secuencia [i+1]) {
+	    if (secuencia[i] > secuencia[i + 1]) {
             // Si encuentra un par de elementos consecutivos que no están ordenados en forma ascendente
 	        return false
 	    }
@@ -15,162 +22,196 @@ fun estaEnOrdenAscendente(secuencia: Array<Int>): Boolean {
     return true
 }
 
-// secuenciaRandom: Int -> Array<Int>
-// Uso: secuenciaRandom(n)
-// Precondición: n > 0
-// Poscondición: Devuelve una secuencia de n elementos con valores aleatorios
+/**
+* secuenciaRandom: Int -> Array<Int>
+* Uso: secuenciaRandom(n)
+* Precondición: n > 0
+* Poscondición: Devuelve una secuencia de n elementos de tipo entero, generados
+* aleatoriamente en el intervalo [0,..., n].
+*/
 fun secuenciaRandom(n: Int): Array<Int> {
     // Crea una secuencia de n elementos cuyos valores iniciales son todos 0
     val secuencia: Array<Int> = Array(n, {0})
     for (i in 0 until n) {
         // Itera sobre los elementos de la secuencia y les asigna un valor aleatorio
-        secuencia[i] =  (0..n).random()
+        secuencia[i] = (0..n).random()
     }
     return secuencia
 }
 
-// secuenciaOrdenada: Int -> Array<Int>
-// Uso: secuenciaOrdenada(n)
-// Precondición: n > 0
-// Poscondición: Devuelve una secuencia de n elementos ordenados en forma ascendente
+/**
+* secuenciaOrdenada: Int -> Array<Int>
+* Uso: secuenciaOrdenada(n)
+* Precondición: n > 0
+* Poscondición: Devuelve una secuencia de n elementos ordenados en forma ascendente
+*/
 fun secuenciaOrdenada(n: Int): Array<Int> {
     // Crea una secuencia de n elementos cuyos valores iniciales son todos 0
     val secuencia: Array<Int> = Array(n, {0})
     for (i in 0 until n) {
         // Itera sobre los elementos de la secuencia y les asigna un valor entre 1 y n
-        secuencia[i] =  i+1
+        secuencia[i] = i + 1
     }
     return secuencia
 }
 
-// secuenciaInvertida: Int -> Array<Int>
-// Uso: secuenciaInvertida(n)
-// Precondición: n > 0
-// Poscondición: Devuelve una secuencia de n elementos ordenados en forma descendente
+/**
+* secuenciaInvertida: Int -> Array<Int>
+* Uso: secuenciaInvertida(n)
+* Precondición: n > 0
+* Poscondición: Devuelve una secuencia de n elementos ordenados en forma descendente
+*/
 fun secuenciaInvertida(n: Int): Array<Int> {
     // Crea una secuencia de n elementos cuyos valores iniciales son todos 0
     val secuencia: Array<Int> = Array(n, {0})
     for (i in 0 until n) {
         // Itera sobre los elementos de la secuencia y les asigna un valor entre 1 y n en forma descendente
-        secuencia[i] =  n-i
+        secuencia[i] = n - i
     }
     return secuencia
 }
 
-// secuenciaCerosYUnos: Int -> Array<Int>
-// Uso: secuenciaCerosYUnos(n)
-// Precondición: n > 0
-// Poscondición: Devuelve una secuencia de n elementos con valores 0 o 1
+/**
+* secuenciaCerosYUnos: Int -> Array<Int>
+* Uso: secuenciaCerosYUnos(n)
+* Precondición: n > 0
+* Poscondición: Devuelve una secuencia de n elementos con valores 0 o 1
+*/
 fun secuenciaCerosYUnos(n: Int): Array<Int> {
     // Crea una secuencia de n elementos cuyos valores iniciales son todos 0
     val secuencia: Array<Int> = Array(n, {0})
     for (i in 0 until n) {
         // Itera sobre los elementos de la secuencia y les asigna un valor aleatorio entre 0 y 1
-        secuencia[i] =  (0..1).random()
+        secuencia[i] = (0..1).random()
     }
     return secuencia
 }
 
-// secuenciaMedia: Int -> Array<Int>
-// Uso: secuenciaMedia(n)
-// Precondición: n > 0
-// Poscondición: Devuelve una secuencia de n elementos con valores en
+/**
+* secuenciaMedia: Int -> Array<Int>
+* Uso: secuenciaMedia(n)
+* Precondición: n > 0
+* Poscondición: Devuelve una secuencia de n elementos de tipo entero, en donde
+* los elementos de la secuencia están ordenados en forma ascendente hasta el
+* elemento n/2 y luego en forma descendente hasta el último elemento. Por
+* ejemplo, si n = 10, la secuencia tiene la forma [1, 2,..., n / 2, n / 2,..., 2, 1].
+*/
 fun secuenciaMedia(n: Int): Array<Int> {
     val secuencia: Array<Int> = Array(n, {0})
-    for (i in 0 until n/2) {
+    for (i in 0 until n / 2) {
         // Itera sobre los elementos de la secuencia entre el primero y el elemento n/2 - 1
-        secuencia[i] =  i+1
+        secuencia[i] = i + 1
     }
-    for (i in n/2 until n) {
+    for (i in n / 2 until n) {
         // Itera sobre los elementos de la secuencia entre el elemento n/2 y el último
-        secuencia[i] =  n-i
+        secuencia[i] = n - i
     }
     return secuencia
 }
 
-// tipoSecuencia: String, Int -> Array<Int>
-// Uso: tipoSecuencia(tipo, n)
-// Precondición: tipo es uno de los siguientes valores: "random", "sorted", "inv", "zu", "media"
-// Poscondición: Devuelve una secuencia de n elementos con valores aleatorios, ordenados, invertidos, con ceros y unos o con valores en forma de media luna
-fun tipoSecuencia(tipo : String, n : Int ): Array<Int> {
-    if(tipo == "random"){
+/**
+* crearSecuencia: String, Int -> Array<Int>
+* Uso: crearSecuencia(tipoSecuencia, n)
+* Precondición: tipoSecuencia es uno de los siguientes valores: "random", "sorted",
+* "inv", "zu", "media"
+* Poscondición: Devuelve una secuencia de n elementos con valores aleatorios,
+* ordenados, invertidos, con ceros y unos o con valores en forma de media luna
+*/
+fun crearSecuencia(tipoSecuencia: String, n: Int ): Array<Int> {
+    if (tipoSecuencia == "random") {
         return secuenciaRandom(n)
-    }
-    else if(tipo == "sorted"){
+    } else if (tipoSecuencia == "sorted") {
         return secuenciaOrdenada(n)
-    }
-    else if(tipo == "inv"){
+    } else if (tipoSecuencia == "inv") {
         return secuenciaInvertida(n)
-    }
-    else if(tipo == "zu"){
+    } else if (tipoSecuencia == "zu") {
         return secuenciaCerosYUnos(n)
-    }
-    else if(tipo == "media"){
+    } else if (tipoSecuencia == "media") {
         return secuenciaMedia(n)
-    }
-    else{
+    } else {
         // Si el tipo de secuencia no es ninguno de los anteriores, devuelve una secuencia ordenada
         println("Error: Tipo de secuencia incorrecto. Se utilizará una secuencia ordenada")
         return secuenciaOrdenada(n)
     }
 }
 
+/**
+* calcularMedia: Array<Long> -> Double
+* Uso: calcularMedia(tiempo)
+* Precondición: tiempo.size > 0
+* Poscondición: Devuelve el promedio de los elementos del arreglo tiempo
+*/
 fun calcularMedia(tiempo: Array<Long>): Double {
-    var suma = 0.0
+    var suma: Double = 0.0
     for (i in 0 until tiempo.size) {
         suma += tiempo[i]
     }
-    return suma/tiempo.size
+    return suma / tiempo.size
 }
 
+/**
+* calcularDesviacionEstandar: Array<Long>, Double -> Double
+* Uso: calcularDesviacionEstandar(tiempo, media)
+* Precondición: tiempo.size > 1
+* Poscondición: Devuelve la desviación estándar de los elementos del arreglo tiempo
+*/
 fun calcularDesviacionEstandar(tiempo: Array<Long>, media: Double): Double {
-    var suma = 0.0
+    var suma: Double = 0.0
     for (i in 0 until tiempo.size) {
         suma += Math.pow((tiempo[i] - media), 2.0)
     }
-    return Math.sqrt(suma/(tiempo.size - 1))
+    return Math.sqrt(suma / (tiempo.size - 1))
 }
 
-fun mensaje(tipo: String, tiempo: Array<Long>) {
-    // Imprime el tiempo promedio de ejecución y la desviación estándar en segundos
-    // Se divide por 1000000000.0 para convertir de nanosegundos a segundos
-    if(tiempo.size > 1){
-        val media : Double = calcularMedia(tiempo)
-        val desviacionEstandar : Double = calcularDesviacionEstandar(tiempo, media)
-        println("\u001b[32mOrdenamiento exitoso con $tipo en ${tiempo.size} intentos \u001b[0m")
-        println("Tiempo promedio de ejecución de $tipo: \u001b[33m${(media/1000000000.0).toDouble()} segundos \u001b[0m")
-        println("Desviación estándar de $tipo: \u001b[33m${(desviacionEstandar/1000000000.0).toDouble()} segundos \u001b[0m")
-    }
-    else{
-        println("\u001b[32mOrdenamiento exitoso con $tipo \u001b[0m")
-        println("Tiempo de ejecución de $tipo: \u001b[33m${(tiempo[0]/1000000000.0).toDouble()} segundos \u001b[0m")
+/**
+* Uso: mensajeOrdenamientoExitoso(tipoDeOrdenamiento, tiempo)
+* Si el usuario especificó más de un intento, se calcula la media y la desviación estándar.
+* Si el usuario especificó un solo intento, se imprime el tiempo de ejecución.
+*/
+fun mensajeOrdenamientoExitoso(tipoDeOrdenamiento: String, tiempo: Array<Long>): Unit {
+    if (tiempo.size > 1) {
+        val media: Double = calcularMedia(tiempo)
+        val desviacionEstandar: Double = calcularDesviacionEstandar(tiempo, media)
+        // Se divide entre 1000000000.0 para convertir el tiempo de nanosegundos a segundos
+        println("\u001b[32mOrdenamiento exitoso con $tipoDeOrdenamiento en ${tiempo.size} intentos \u001b[0m")
+        println("Tiempo promedio de ejecución de $tipoDeOrdenamiento: \u001b[33m${(media/1000000000.0).toDouble()} segundos \u001b[0m")
+        println("Desviación estándar de $tipoDeOrdenamiento: \u001b[33m${(desviacionEstandar/1000000000.0).toDouble()} segundos \u001b[0m")
+    } else {
+        println("\u001b[32mOrdenamiento exitoso con $tipoDeOrdenamiento \u001b[0m")
+        println("Tiempo de ejecución de $tipoDeOrdenamiento: \u001b[33m${(tiempo[0]/1000000000.0).toDouble()} segundos \u001b[0m")
     }
     println("----------------------------------------------------------------------------------------")
 }
 
-fun error(tipo: String) {
-    println("\u001b[31mError: La secuencia no está ordenada con $tipo \u001b[0m")
+/**
+* Uso: mensajeErrorEnElOrdenamiento(tipoDeOrdenamiento)
+* Imprime un mensaje de error indicando que la secuencia no está ordenada con el tipo de
+* ordenamiento especificado
+*/
+fun mensajeErrorEnElOrdenamiento(tipoDeOrdenamiento: String): Unit {
+    println("\u001b[31mError: La secuencia no está ordenada con $tipoDeOrdenamiento \u001b[0m")
     println("----------------------------------------------------------------------------------------")
 }
 
-fun main(args: Array<String>) {
+// Declaración del método principal
+fun main(args: Array<String>): Unit {
     if(args.size != 6){
         // Si la cantidad de argumentos es distinta de 6, imprime un mensaje de error y termina la ejecución
         println("Error: Cantidad de argumentos incorrecta")
-        println("Uso: ./main -n <tamaño> -t <intentos> -s <tipo>")
+        println("Uso: ./main -n <tamaño> -t <intentos> -s <tipoSecuencia>")
         return
     }
     // Crea un diccionario con los argumentos pasados por línea de comandos
-    // Ejemplo: ./main -n 100 -t 10 -s random
+    // Ejemplo: ./runSortlib.sh -n 100 -t 10 -s random
     val consts = mapOf(args[0] to args[1], args[2] to args[3], args[4] to args[5])
     // Obtiene los valores de los argumentos
     val n : Int = consts["-n"]!!.toInt()
     val intentos : Int = consts["-t"]!!.toInt()
-    val tipo = consts["-s"]!!
+    val tipoSecuencia = consts["-s"]!!
 
     // Crea una secuencia según el tipo y el tamaño pasados por línea de comandos
-    val secuencia = tipoSecuencia(tipo, n)
-    println("\u001b[100mSecuencia de tamaño $n generada de tipo $tipo\u001b[0m")
+    val secuencia = crearSecuencia(tipoSecuencia, n)
+    println("\u001b[36mSecuencia de tamaño $n generada de tipo $tipoSecuencia\u001b[0m")
     println("----------------------------------------------------------------------------------------")
 
     // Se realizan intentos de ordenamiento con Bubble Sort
@@ -185,14 +226,14 @@ fun main(args: Array<String>) {
         // Se verifica si la secuencia copia está ordenada
         if(!estaEnOrdenAscendente(secuenciaCopia)){
             // Si la secuencia copia no está ordenada, imprime un mensaje de error y termina la ejecución
-            error("Bubble Sort")
+            mensajeErrorEnElOrdenamiento("Bubble Sort")
             return
         }
         // Se guarda el tiempo de ejecución
         tiempoBubbleSort[i] = (tiempoFinal - tiempoInicial)
     }
     // Si la secuencia está ordenada en todos los intentos, imprime un mensaje de éxito
-    mensaje("Bubble Sort", tiempoBubbleSort)
+    mensajeOrdenamientoExitoso("Bubble Sort", tiempoBubbleSort)
 
     // Se realizan intentos de ordenamiento con Insertion Sort y se mide el tiempo de ejecución
     val tiempoInsertionSort = Array<Long>(intentos, {0})
@@ -206,14 +247,14 @@ fun main(args: Array<String>) {
         // Se verifica si la secuencia copia está ordenada
         if(!estaEnOrdenAscendente(secuenciaCopia)){
             // Si la secuencia copia no está ordenada, imprime un mensaje de error y termina la ejecución
-            error("Insertion Sort")
+            mensajeErrorEnElOrdenamiento("Insertion Sort")
             return
         }
         // Se guarda el tiempo de ejecución
         tiempoInsertionSort[i] = (tiempoFinal - tiempoInicial)
     }
     // Si la secuencia está ordenada en todos los intentos, imprime un mensaje de éxito
-    mensaje("Insertion Sort", tiempoInsertionSort)
+    mensajeOrdenamientoExitoso("Insertion Sort", tiempoInsertionSort)
 
     // Se realizan intentos de ordenamiento con Selection Sort y se mide el tiempo de ejecución
     val tiempoSelectionSort = Array<Long>(intentos, {0})
@@ -227,14 +268,14 @@ fun main(args: Array<String>) {
         // Se verifica si la secuencia copia está ordenada
         if(!estaEnOrdenAscendente(secuenciaCopia)){
             // Si la secuencia copia no está ordenada, imprime un mensaje de error y termina la ejecución
-            error("Selection Sort")
+            mensajeErrorEnElOrdenamiento("Selection Sort")
             return
         }
         // Se guarda el tiempo de ejecución
         tiempoSelectionSort[i] = (tiempoFinal - tiempoInicial)
     }
     // Si la secuencia está ordenada en todos los intentos, imprime un mensaje de éxito
-    mensaje("Selection Sort", tiempoSelectionSort)
+    mensajeOrdenamientoExitoso("Selection Sort", tiempoSelectionSort)
 
     // Se realizan intentos de ordenamiento con Shell Sort y se mide el tiempo de ejecución
     val tiempoShellSort = Array<Long>(intentos, {0})
@@ -248,12 +289,12 @@ fun main(args: Array<String>) {
         // Se verifica si la secuencia copia está ordenada
         if(!estaEnOrdenAscendente(secuenciaCopia)){
             // Si la secuencia copia no está ordenada, imprime un mensaje de error y termina la ejecución
-            error("Shell Sort")
+            mensajeErrorEnElOrdenamiento("Shell Sort")
             return
         }
         // Se guarda el tiempo de ejecución
         tiempoShellSort[i] = (tiempoFinal - tiempoInicial)
     }
     // Si la secuencia está ordenada en todos los intentos, imprime un mensaje de éxito
-    mensaje("Shell Sort", tiempoShellSort)
+    mensajeOrdenamientoExitoso("Shell Sort", tiempoShellSort)
 }
--- lab-sem-1/codigo/Sortlib.kt
+++ lab-sem-1/codigo/Sortlib.kt
@@ -1,9 +1,26 @@
+/*
+Laboratorio de la semana 1 de Algoritmos y Estructuras de Datos II (CI-2692)
+Autores: Juan Cuevas (19-10056) y Luis Isea (19-10175)
+*/
+
+/**
+* uso: swap(A, i, j)
+* Precondición: A es un arreglo de enteros, i y j son índices válidos en A
+* Postcondición: intercambia los elementos en las posiciones i y j del arreglo A
+*/
 fun swap(A: Array<Int>, i: Int, j: Int): Unit {
     val aux = A[i]
     A[i] = A[j]
     A[j] = aux
 }
 
+/**
+* uso: bubbleSort(A)
+* Precondición: A es un arreglo de enteros
++ Postcondición: ordena los elementos de A de menor a mayor
+* Descripcióm: compara dos elementos adyacentes y los intercambia si están
+* en el orden equivocado
+*/
 fun bubbleSort(A: Array<Int>): Unit {
     for (i in 0 until A.size - 1) {
         for (j in (A.size - 1) downTo (i + 1)) {
@@ -14,6 +31,13 @@ fun bubbleSort(A: Array<Int>): Unit {
     }
 }
 
+/**
+* uso: insertionSort(A)
+* Precondición: A es un arreglo de enteros
+* Postcondición: ordena los elementos de A de menor a mayor
+* Descripción: inserta un elemento en la posición correcta de lo se haya ordenado,
+* empujando los elementos mayores a la derecha
+*/
 fun insertionSort(A: Array<Int>): Unit {
     for (i in 1 until A.size) {
         var j = i
@@ -24,6 +48,13 @@ fun insertionSort(A: Array<Int>): Unit {
     }
 }
 
+/**
+* uso: selectionSort(A)
+* Precondición: A es un arreglo de enteros
+* Postcondición: ordena los elementos de A de menor a mayor
+* Descripción: busca el elemento más pequeño de la parte no ordenada y lo intercambia
+* con el primer elemento de la parte no ordenada
+*/
 fun selectionSort(A: Array<Int>): Unit {
     for (i in 0 until A.size - 1) {
         var min = i
@@ -36,6 +67,13 @@ fun selectionSort(A: Array<Int>): Unit {
     }
 }
 
+/**
+* uso: shellSort(A)
+* Precondición: A es un arreglo de enteros
+* Postcondición: ordena los elementos de A de menor a mayor
+* Descripción: primero ordena elementos que estén muy separados entre sí, y
+* posteriormente reduce la separación entre los elementos a ordenar
+*/
 fun shellSort(A: Array<Int>): Unit {
     var incr: Int = A.size / 2
     while (incr > 0) {
