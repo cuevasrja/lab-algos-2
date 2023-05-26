46fb43869ec78aad22ac4493d8b29c4a964f0d02
Luis Isea <19-10175@usb.ve>
2023-05-26 16:46:38 -0400

    Reestructurado lab-sem-3

--- lab-sem-3/runSortlib.sh
+++ lab-sem-3/runSortlib.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Laboratorio de la semana 2 de Algoritmos y Estructuras de Datos II (CI-2692).
+# Laboratorio de la semana 3 de Algoritmos y Estructuras de Datos II (CI-2692).
 # Autores: Juan Cuevas (19-10056) y Luis Isea (19-10175).
 
 java -jar Sortlib.jar $*
