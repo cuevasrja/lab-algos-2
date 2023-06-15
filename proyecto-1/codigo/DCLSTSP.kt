c45e8cca1e2a732b23b7e8e8d2689e4cee984707
Juan Cuevas <juanandrescuevas14@gmail.com>
2023-06-15 13:53:04 -0400

    aiuda

--- proyecto-1/codigo/DCLSTSP.kt
+++ proyecto-1/codigo/DCLSTSP.kt
@@ -1,5 +1,7 @@
 fun obtenerPuntoDeCorte(P: Array<Pair<Int, Int>>, eje: Int): Pair<Int, Int>{
-
+    val n = P.size
+    val pos = Math.ceil(n/2.0).toInt() - 1
+    
 }
 
 fun obtenerPuntoDeCorteMitad(rectangulo: Array<Pair<Int, Int>>, eje: Int): Pair<Int, Int>{
@@ -10,7 +12,7 @@ fun aplicarCorte(eje: Int, puntoDeCorte: Pair<Int, Int>, rectangulo: Array<Pair<
     
 }
 
-fun obtenerParticiones(P: Array<Pair<Int, Int>>): Array<Array<Pair<Int, Int>>>{
+fun obtenerParticiones(P: Array<Pair<Int, Int>>): Pair<Array<Int>, Array<Int>>{
     
 }
 
@@ -36,11 +38,33 @@ fun distanciaGanada(n1: Int, n2: Int, o1: Int, o2: Int): Int{
 }
 
 fun divideAndConquerAndLocalSearchTSP(P: Array<Pair<Int, Int>>): Array<Pair<Pair<Int, Int>, Pair<Int, Int>>>{
-    
+    return
+}
+
+fun combinarCiclos(c1: Array<Pair<Pair<Int, Int>, Pair<Int, Int>>>, c2: Array<Pair<Pair<Int, Int>, Pair<Int, Int>>>): Array<Pair<Pair<Int, Int>, Pair<Int, Int>>>{
+    return
 }
 
 fun divideAndConquerTSP (P: Array<Pair<Int, Int>>): Array<Pair<Pair<Int, Int>, Pair<Int, Int>>>{
-
+    val n = P.size
+    if (n == 0){
+        return arrayOf()
+    }
+    else if (n == 1){
+        return arrayOf(Pair(P[0], P[0]))
+    }
+    else if (n == 2){
+        return arrayOf(Pair(P[0], P[1]), Pair(P[1], P[0]))
+    }
+    else if (n == 3){
+        return
+    }
+    else{
+        val (pright, pleft) = obtenerParticiones(P)
+        val c1 = divideAndConquerTSP(pright)
+        val c2 = divideAndConquerTSP(pleft)
+        return combinarCiclos(c1, c2)
+    }
 }
 
 
