356e1bdf69529dea001e0616ccc88bbc7b7dd152
Luis Isea <19-10175@usb.ve>
2023-06-25 18:17:37 -0400

    Mejoradas las pruebas y añadido la prueba cuando lista.buscar() retorne null

--- lab-sem-7/Main.kt
+++ lab-sem-7/Main.kt
@@ -2,6 +2,7 @@
 // Autores: Juan Cuevas (19-10056) y Luis Isea (19-10175).
 
 fun main() {
+    // Creamos un arreglo con 10 elementos aleatorios entre 0 y 100
     println("\u001b[36mSe creó un arreglo con 10 elementos aleatorios entre 0 y 100.\u001b[0m")
     val prueba: Array<Int> = Array(4, {0})
     for (i in 0 until prueba.size) {
@@ -10,6 +11,7 @@ fun main() {
     println("Arreglo original: ${prueba.contentToString()}")
     println()
 
+    // Probamos la lista circular
     println("\u001b[36mPrueba de la lista circular:\u001b[0m")
     println("----------------------------------------------------------------")
     val lista: ListaCircular = crearListaCircular()
@@ -21,12 +23,14 @@ fun main() {
     println("Imprimimos el primer elemento de la lista: \u001b[34m${lista.primerElemento()}\u001b[0m.")
     println("Imprimimos el ultimo elemento de la lista: \u001b[34m${lista.ultimoElemento()}\u001b[0m.")
     println("Imprimimos el elemento en la posicion \u001b[34m2\u001b[0m de la lista: \u001b[34m${lista.buscarPorIndice(2)}\u001b[0m.")
-    println("Añadimos el elemento 100 al principio de la lista.")
-    lista.agregarAlFrente(100)
+    println("Buscamos un elemento que no existe, como el elemento \u001b[34m-1\u001b[0m: \u001b[34m${lista.buscar(-1)}\u001b[0m.")
+    println("Añadimos el elemento 200 al principio de la lista.")
+    lista.agregarAlFrente(200)
     println("Lista modificada: \u001b[33m$lista\u001b[0m")
-    println("Añadimos el elemento 200 al final de la lista.")
-    lista.agregarAlFinal(200)
+    println("Añadimos el elemento 100 al final de la lista.")
+    lista.agregarAlFinal(100)
     println("Lista modificada: \u001b[33m$lista\u001b[0m")
+    println("Buscamos la primera aparición del elemento \u001b[34m100\u001b[0m en la lista: \u001b[34m${lista.obtenerIndice(lista.buscar(100)!!.obtenerDato()!!)}\u001b[0m.")
     val elemAleatorio = lista.buscarPorIndice((0 until lista.getSize()).random())
     val posElemAleatorio = lista.obtenerIndice(elemAleatorio!!.obtenerDato()!!)
     println("Obtenemos un elemento aleatorio de la lista: \u001b[34m$elemAleatorio\u001b[0m, que está en la posicion \u001b[34m$posElemAleatorio\u001b[0m.")
@@ -41,6 +45,7 @@ fun main() {
     println("Lista modificada: \u001b[33m$lista\u001b[0m")
     println()
 
+    // Probamos la pila
     println("\u001b[36mPrueba de la pila:\u001b[0m")
     println("----------------------------------------------------------------")
     val pila: Pila = crearPila(prueba.size)
@@ -49,11 +54,11 @@ fun main() {
         pila.empilar(i)
     }
     println("Pila original: \u001b[33m$pila\u001b[0m")
-    println("Imprimimos el tope de la pila: ${pila.tope()}")
+    println("Imprimimos el tope de la pila: \u001b[34m${pila.tope()}\u001b[0m.")
     println("Agremos el elemento 100 al tope de la pila.")
     pila.empilar(100)
     println("Pila modificada: \u001b[33m$pila\u001b[0m")
-    println("Imprimimos el tope de la pila: ${pila.tope()}")
+    println("Imprimimos el tope de la pila: \u001b[34m${pila.tope()}\u001b[0m.")
     for (i in 0 until prueba.size) {
         println("Desapilamos el tope de la pila.")
         pila.desempilar()
@@ -62,6 +67,7 @@ fun main() {
     }
     println()
 
+    // Probamos la cola
     println("\u001b[36mPrueba de la cola:\u001b[0m")
     println("----------------------------------------------------------------")
     val cola: Cola = crearCola(prueba.size)
@@ -70,10 +76,11 @@ fun main() {
         cola.encolar(i)
     }
     println("Cola original: \u001b[33m$cola\u001b[0m")
-    println("Imprimimos el primero de la cola: ${cola.primero()}")
+    println("Imprimimos el primero de la cola: \u001b[34m${cola.primero()}\u001b[0m.")
     println("Agregamos el elemento 100 al final de la cola.")
     cola.encolar(100)
     println("Cola modificada: \u001b[33m$cola\u001b[0m")
+    println("Imprimimos el primero de la cola: \u001b[34m${cola.primero()}\u001b[0m.")
     for (i in 0 until prueba.size) {
         println("Desencolamos el primero de la cola.")
         cola.desencolar()
