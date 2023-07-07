1284e958b734c00e6dd59b8c37aa93214d8819cb
Juan Cuevas <juanandrescuevas14@gmail.com>
2023-07-07 11:17:40 -0400

    Codigo Morse

--- lab-sem-9/ArbolBinario.kt
+++ lab-sem-9/ArbolBinario.kt
@@ -5,6 +5,110 @@
  * ArbolBinario.kt -> Este archivo contiene el arbol binario de busqueda del alfabeto en morse.
  */
 
-class ArbolBinario{
+class ArbolBinario(){
+    var raiz: Nodo? = null
+
+    fun agregar(nuevo: Nodo){
+        if (raiz == null){
+            raiz = nuevo
+        } else {
+            agregar(nuevo, raiz!!)
+        }
+    }
+
+    fun agregar(nuevo: Nodo, nodo: Nodo){
+        val nuevoValor = codigoToInt(nuevo.getCodigo())
+        val nodoValor = codigoToInt(nodo.getCodigo())
+        if (nuevoValor < nodoValor){
+            if (nodo.izq == null){
+                nodo.izq = nuevo
+            } else {
+                agregar(nuevo, nodo.izq!!)
+            }
+        } else {
+            if (nodo.der == null){
+                nodo.der = nuevo
+            } else {
+                agregar(nuevo, nodo.der!!)
+            }
+        }
+    }
+
+    fun buscarCodigo(valor: Char): String{
+        return busquedaCodigo(valor, raiz!!)
+    }
+
+    private fun busquedaCodigo(valor: Char, nodo: Nodo): String{
+        if (nodo.valor == valor){
+            return nodo.codigo
+        } else {
+            if (valor < nodo.valor){
+                return busquedaCodigo(valor, nodo.izq!!)
+            } else {
+                return busquedaCodigo(valor, nodo.der!!)
+            }
+        }
+    }
+
+    fun buscar(codigo: String): Char{
+        return busqueda(codigo, raiz!!)
+    }
+
+    private fun busqueda(codigo: String, nodo: Nodo): Char{
+        val codigoNodo = nodo.getCodigo()
+        val codigoNodoInt = codigoToInt(codigoNodo)
+        val codigoInt = codigoToInt(codigo)
+        if (codigoNodoInt == codigoInt){
+            return nodo.getValor()
+        } else {
+            if (codigoInt < codigoNodoInt){
+                return busqueda(codigo, nodo.izq!!)
+            } else {
+                return busqueda(codigo, nodo.der!!)
+            }
+        }
+
+    }
     
+    private fun codigoToInt(codigo: String): Int{
+        return codigo.replace(".", "0").replace("-", "1").toInt(2)
+    }
+}
+
+class Nodo(val valor: Char, val codigo: String){
+    var izq: Nodo? = null
+    var der: Nodo? = null
+    var padre: Nodo? = null
+
+    fun esHoja(): Boolean{
+        return izq == null && der == null
+    }
+
+    fun esIzquierdo(): Boolean{
+        return padre != null && padre!!.izq == this
+    }
+
+    fun esDerecho(): Boolean{
+        return padre != null && padre!!.der == this
+    }
+
+    fun esRaiz(): Boolean{
+        return padre == null
+    }
+
+    fun esHijo(): Boolean{
+        return esIzquierdo() || esDerecho()
+    }
+
+    fun esPadre(): Boolean{
+        return izq != null || der != null
+    }
+
+    fun getCodigo(): String{
+        return codigo
+    }
+
+    fun getValor(): Char{
+        return valor
+    }
 }
--- lab-sem-9/CodigoMorse.kt
+++ lab-sem-9/CodigoMorse.kt
@@ -5,6 +5,69 @@
  * CodigoMorse.kt -> Este archivo descifra el codigo morse que recibe mediante el archivo PruebaCodigoMorse.kt.
  */
 
- class CodigoMorse {
-    
- }
+class CodigoMorse(){
+    // El alfabeto en morse se encuentra en un arbol binario de busqueda.
+    private val alfabeto = ArbolBinario()
+
+    // Se inicializa el arbol binario con el alfabeto en morse.
+    init {
+        alfabeto.agregar(Nodo('E', "."))
+        alfabeto.agregar(Nodo('T', "-"))
+        alfabeto.agregar(Nodo('I', ".."))
+        alfabeto.agregar(Nodo('A', ".-"))
+        alfabeto.agregar(Nodo('N', "-."))
+        alfabeto.agregar(Nodo('M', "--"))
+        alfabeto.agregar(Nodo('S', "..."))
+        alfabeto.agregar(Nodo('U', "..-"))
+        alfabeto.agregar(Nodo('R', ".-."))
+        alfabeto.agregar(Nodo('W', ".--"))
+        alfabeto.agregar(Nodo('D', "-.."))
+        alfabeto.agregar(Nodo('K', "-.-"))
+        alfabeto.agregar(Nodo('G', "--."))
+        alfabeto.agregar(Nodo('O', "---"))
+        alfabeto.agregar(Nodo('H', "...."))
+        alfabeto.agregar(Nodo('V', "...-"))
+        alfabeto.agregar(Nodo('F', "..-."))
+        alfabeto.agregar(Nodo('L', ".-.."))
+        alfabeto.agregar(Nodo('P', ".--."))
+        alfabeto.agregar(Nodo('J', ".---"))
+        alfabeto.agregar(Nodo('B', "-..."))
+        alfabeto.agregar(Nodo('X', "-..-"))
+        alfabeto.agregar(Nodo('C', "-.-."))
+        alfabeto.agregar(Nodo('Y', "-.--"))
+        alfabeto.agregar(Nodo('Z', "--.."))
+        alfabeto.agregar(Nodo('Q', "--.-"))
+    }
+    /** 
+    * fun decodificarLetra(codigo: String): Char
+    * Metodo que recibe un codigo morse y devuelve la letra correspondiente.
+    * @param codigo: String -> Codigo morse a decodificar.
+    * @return Char -> Letra correspondiente al codigo morse.
+    * Precondicion: El codigo morse debe ser valido.
+    * Postcondicion: Se devuelve la letra correspondiente al codigo morse.
+    */
+    fun decodificarLetra(codigo: String): Char{
+        return alfabeto.buscar(codigo)
+    }
+
+    /**
+    * fun decodificarMensaje(mensaje: String): String
+    * Metodo que recibe un mensaje en codigo morse y devuelve el mensaje en texto.
+    * @param mensaje: String -> Mensaje en codigo morse a decodificar.
+    * @return String -> Mensaje en texto correspondiente al mensaje en codigo morse.
+    * Precondicion: El mensaje en codigo morse debe ser valido.
+    * Postcondicion: Se devuelve el mensaje en texto correspondiente al mensaje en codigo morse.
+    */
+    fun decodificarMensaje(mensaje: String): String{
+        val palabras = mensaje.toUpperCase().split("/")
+        var mensajeDecodificado = ""
+        for (palabra in palabras){
+            val letras = palabra.trim().split(" ")
+            for (letra in letras){
+                mensajeDecodificado += decodificarLetra(letra)
+            }
+            mensajeDecodificado += " "
+        }
+        return mensajeDecodificado.trim().toLowerCase()
+    }
+}
