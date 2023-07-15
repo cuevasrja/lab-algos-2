cdecb7d96292a90008ba4cd046600621803b6fc5
Juan Cuevas <juanandrescuevas14@gmail.com>
2023-07-14 23:01:46 -0400

    Colorsito

--- proyecto-2/PruebaOrtografia.kt
+++ proyecto-2/PruebaOrtografia.kt
@@ -15,12 +15,12 @@ import kotlin.system.exitProcess
  * Postcondición: se imprime un mensaje de bienvenida al usuario.
  */
 fun imprimirMensajeBienvenida() {
-    println("¡Disfruta de este increíble Asistente Ortográfico!")
+    println("\u001b[32m¡Disfruta de este increíble Asistente Ortográfico!\u001b[0m")
     println("Este programa te ayudará a corregir tus textos.\n")
     println("Para ello, debes ingresar un diccionario con las palabras que consideres válidas.")
     println("Luego, ingresa el texto que deseas corregir y el programa te mostrará las palabras\nque no se encuentran en el diccionario y sus respectivas sugerencias.\n")
     println("¡Esperamos que disfrutes de esta experiencia!")
-    println("Creado por: Juan Cuevas (19-10056) y Luis Isea (19-10175).\n")
+    println("Creado por: \u001b[33mJuan Cuevas (19-10056)\u001b[0m y \u001b[33mLuis Isea (19-10175).\u001b[0m\n")
 }
 
 /**
@@ -31,10 +31,13 @@ fun imprimirMensajeBienvenida() {
  */
 fun imprimirOpciones() {
     println("Puedes realizar las siguientes operaciones:")
-    println("1. Crear un nuevo ayudante ortográfico.")
-    println("2. Cargar un diccionario. 3. Eliminar una palabra del diccionario.")
-    println("4. Corregir texto. 5. Mostrar diccionario. 6. Salir de la aplicación.")
-    print("¿Qué operación deseas realizar?: ")
+    println("\u001b[34m1.\u001b[0m Crear un nuevo ayudante ortográfico.")
+    println("\u001b[34m2.\u001b[0m Cargar un diccionario.")
+    println("\u001b[34m3.\u001b[0m Eliminar una palabra del diccionario.")
+    println("\u001b[34m4.\u001b[0m Corregir texto.")
+    println("\u001b[34m5.\u001b[0m Mostrar diccionario.")
+    println("\u001b[34m6.\u001b[0m Salir de la aplicación.")
+    print("\u001b[33m¿Qué operación deseas realizar?:\u001b[0m ")
 }
 
 /**
@@ -59,20 +62,20 @@ fun main() {
             2 -> {
                 println("El diccionario debe ser un archivo de texto con una palabra por línea.")
                 println("Las palabras deben estar en minúsculas y sin acentos.")
-                println("Ingrese el nombre del archivo que contiene el diccionario:")
+                println("\u001b[34mIngrese el nombre del archivo que contiene el diccionario:\u001b[0m")
                 val fname = readLine()!!
                 ayudante.cargarDiccionario(fname)
             }
             3 -> {
-                print("Ingrese la palabra que deseas eliminar del diccionario: ")
+                print("\u001b[34mIngrese la palabra que deseas eliminar del diccionario:\u001b[0m ")
                 val palabra = readLine()!!
                 ayudante.borrarPalabra(palabra)
             }
             4 -> {
-                println("Ingrese el nombre del archivo que contiene el texto:")
+                println("\u001b[34mIngrese el nombre del archivo que contiene el texto:\u001b[0m")
                 val fname = readLine()!!
-                println("Ingrese el nombre del archivo donde desea guardar las sugerecias de corrección:")
-                println("(El archivo de salida se creará si no existe)")
+                println("\u001b[34mIngrese el nombre del archivo donde desea guardar las sugerecias de corrección:\u001b[0m")
+                println("\u001b[31;3m(El archivo de salida se creará si no existe)\u001b[0m")
                 val fname2 = readLine()!!
                 ayudante.corregirTexto(fname, fname2)
             }
@@ -80,11 +83,11 @@ fun main() {
                 ayudante.imprimirDiccionario()
             }
             6 -> {
-                println("¡Hasta luego!")
+                println("\u001b[34m¡Hasta luego!\u001b[0m")
                 salir = true
             }
             else -> {
-                println("Opción inválida. Escriba un número entre 1 y 6.\n")
+                println("\u001b[31mOpción inválida. Escriba un número entre 1 y 6.\u001b[0m\n")
             }
         }
     }
